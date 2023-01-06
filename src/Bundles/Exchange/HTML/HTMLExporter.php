45563e2e039e71309e9580ecc67a39230d4aea82
Christian Dangl <boxblinkracer@gmx.net>
2023-01-07 00:19:31 +0100

    fix broken header column stickiness in html

--- src/Bundles/Exchange/HTML/HTMLExporter.php
+++ src/Bundles/Exchange/HTML/HTMLExporter.php
@@ -159,7 +159,14 @@ td, th {
 
 th {
   background-color: #eee;
- 
+  position: sticky;
+  top: -1px;
+  z-index: 2;
+  
+  &:first-of-type {
+    left: 0;
+    z-index: 3;
+  }
 }
 
 tbody tr td:first-of-type {
