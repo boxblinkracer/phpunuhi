bb57d43772fa03714d6a6dc962b5b43a491900c5
Christian Dangl <boxblinkracer@gmx.net>
2023-01-19 23:31:34 +0100

    remove unused function

--- src/Traits/CommandTrait.php
+++ src/Traits/CommandTrait.php
@@ -3,9 +3,7 @@
 namespace PHPUnuhi\Traits;
 
 use PHPUnuhi\PHPUnuhi;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 
 trait CommandTrait
 {
@@ -36,19 +34,6 @@ trait CommandTrait
         $workingDir = $cur_dir[count($cur_dir) - 1];
 
         return $workingDir . '/' . $configFile;
-    }
-
-    /**
-     * @param Command $command
-     * @return void
-     */
-    protected function addTranslatorServiceOptions(Command $command)
-    {
-        $command->addOption('service', null, InputOption::VALUE_REQUIRED, '', '');
-
-        $command->addOption('google-key', null, InputOption::VALUE_REQUIRED, '', '');
-        $command->addOption('deepl-key', null, InputOption::VALUE_REQUIRED, '', '');
-        $command->addOption('openai-key', null, InputOption::VALUE_REQUIRED, '', '');
     }
 
 }
