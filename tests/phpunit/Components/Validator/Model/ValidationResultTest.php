d18f753131085a19ec6e090067a3f3c22ee18821
Christian Dangl <cd@dasistweb.de>
2023-12-18 22:36:01 +0100

    add validation result unit tests

---
+++ tests/phpunit/Components/Validator/Model/ValidationResultTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace phpunit\Components\Validator\Model;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnuhi\Components\Validator\Model\ValidationError;
+use PHPUnuhi\Components\Validator\Model\ValidationResult;
+use PHPUnuhi\Components\Validator\Model\ValidationTest;
+
+class ValidationResultTest extends TestCase
+{
+
+
+    /**
+     * @var ValidationResult
+     */
+    private $result;
+
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $test = new ValidationTest(
+            '',
+            '',
+            '',
+            '',
+            5,
+            '',
+            '',
+            true
+        );
+
+        $error = new ValidationError(
+            '',
+            '',
+            '',
+            '',
+            '',
+            4
+        );
+
+        $this->result = new ValidationResult([$test], [$error]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetTests()
+    {
+        $this->assertCount(1, $this->result->getTests());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetErrors()
+    {
+        $this->assertCount(1, $this->result->getErrors());
+    }
+
+}
