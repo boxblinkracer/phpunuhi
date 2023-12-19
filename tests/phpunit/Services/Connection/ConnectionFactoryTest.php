80e1aa1b8bed4a525af6d9a10aa4c2de4cf21f47
Christian Dangl <cd@dasistweb.de>
2023-12-19 21:49:42 +0100

    add pdo unit test

---
+++ tests/phpunit/Services/Connection/ConnectionFactoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace phpunit\Services\Connection;
+
+use PDO;
+use PHPUnit\Framework\TestCase;
+use PHPUnuhi\Services\Connection\ConnectionFactory;
+
+class ConnectionFactoryTest extends TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function testPdoObjectBuiltCorrectly(): void
+    {
+        putenv('DB_HOST=127.0.01');
+        putenv('DB_PORT=3306');
+        putenv('DB_USER=test');
+        putenv('DB_PASSWD=test');
+        putenv('DB_DBNAME=test');
+
+        $this->expectExceptionMessage('SQLSTATE[HY000] [2002] Connection refused');
+
+        $factory = new ConnectionFactory();
+        $conn = $factory->pdoFromEnv();
+
+        $this->assertInstanceOf(PDO::class, $conn);
+    }
+
+}
