7a59b0313f2f100feb6d23c27f6dfc332aeee971
Christian Dangl <cd@dasistweb.de>
2023-12-21 22:41:41 +0100

    configure cs fixer to remove unused imports

--- .php_cs.php
+++ .php_cs.php
@@ -5,9 +5,11 @@ return PhpCsFixer\Config::create()
     ->setRules([
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => true,
+        'no_unused_imports' => true,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->exclude(['./src/vendor'])
             ->in(__DIR__ . '/src')
+            ->in(__DIR__ . '/tests/phpunit')
     );
