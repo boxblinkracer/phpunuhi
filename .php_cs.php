af7208c777a6dccd4899064846bcd3290626de4b
Christian Dangl <cd@dasistweb.de>
2023-12-21 22:52:21 +0100

    update php cs fixer config

--- .php_cs.php
+++ .php_cs.php
@@ -3,9 +3,19 @@
 return PhpCsFixer\Config::create()
     ->setUsingCache(false)
     ->setRules([
-        'array_syntax' => ['syntax' => 'short'],
+        '@PSR2' => true,
         'ordered_imports' => true,
         'no_unused_imports' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'align_multiline_comment' => true,
+        'array_indentation' => true,
+        'no_superfluous_elseif' => true,
+        'not_operator_with_successor_space' => false,
+        'phpdoc_add_missing_param_annotation' => true,
+        'phpdoc_order' => true,
+        'phpdoc_trim_consecutive_blank_line_separation' => true,
+        'phpdoc_types_order' => true,
+        'yoda_style' => false,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
