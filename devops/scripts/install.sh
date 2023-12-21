f1066d68f7d1de543ededea6c7c281d284c412be
Christian Dangl <boxblinkracer@gmx.net>
2023-12-21 20:35:46 +0100

    upgrade to php 8.2 for dev

---
+++ devops/scripts/install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y zip
+sudo apt-get install -y unzip
+
+
+sudo chmod 777 /etc/php/8.2/cli/php.ini
+sudo echo 'phar.readonly => 0' >  /etc/php/8.2/cli/php.ini
+
+
+cd /var/www && make restart-php
