9f8f62791857a6ef9436c729a013c0c416c68e0b
Christian Dangl <cd@dasistweb.de>
2023-12-23 10:23:27 +0100

    add unit tests for configuration

---
+++ tests/phpunit/Components/Configuration/Services/StyleLoaderTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace phpunit\Components\Configuration\Services;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnuhi\Configuration\Services\StyleLoader;
+use PHPUnuhi\Models\Configuration\CaseStyle;
+
+class StyleLoaderTest extends TestCase
+{
+
+    /**
+     * @return void
+     */
+    public function testLoadStyles(): void
+    {
+        $xmlString = <<<XML
+<styles>
+    <style level="1">camel</style>
+    <style>pascal</style>
+    <style level="2">kebab</style>
+</styles>
+XML;
+
+        $xml = simplexml_load_string($xmlString);
+
+
+        $loader = new StyleLoader();
+        $result = $loader->loadStyles($xml);
+
+
+        $this->assertCount(3, $result);
+
+        $this->assertInstanceOf(CaseStyle::class, $result[0]);
+        $this->assertInstanceOf(CaseStyle::class, $result[1]);
+        $this->assertInstanceOf(CaseStyle::class, $result[2]);
+
+        $this->assertEquals('camel', $result[0]->getName());
+        $this->assertEquals(1, $result[0]->getLevel());
+
+        $this->assertEquals('pascal', $result[1]->getName());
+        $this->assertEquals(-1, $result[1]->getLevel());
+
+        $this->assertEquals('kebab', $result[2]->getName());
+        $this->assertEquals(2, $result[2]->getLevel());
+    }
+
+    /**
+     * @return void
+     */
+    public function testLoadWithoutStylesNode(): void
+    {
+        $xmlString = <<<XML
+<root>
+</root>
+XML;
+
+        $xml = simplexml_load_string($xmlString);
+
+        $loader = new StyleLoader();
+        $result = $loader->loadStyles($xml);
+
+        $this->assertCount(0, $result);
+    }
+
+    /**
+     * @return void
+     */
+    public function testLoadWithInvalidStyle(): void
+    {
+        $xmlString = <<<XML
+<styles>
+    <style>pascal</style>
+    <style>abc</style>
+</styles>
+XML;
+
+        $xml = simplexml_load_string($xmlString);
+
+        $loader = new StyleLoader();
+        $result = $loader->loadStyles($xml);
+
+        $this->assertCount(2, $result);
+    }
+}
