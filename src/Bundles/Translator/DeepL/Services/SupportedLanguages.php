1a64cdc0dce6f13c7a9b50a4cf443369e4dadaab
Tobias Matthaiou <225997+TumTum@users.noreply.github.com>
2024-05-01 16:29:15 +0200

    The DeepL Translator checks the availability of the target language. It doesn't matter whether the country code is included or not.

---
+++ src/Bundles/Translator/DeepL/Services/SupportedLanguages.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace PHPUnuhi\Bundles\Translator\DeepL\Services;
+
+use DeepL;
+use DeepL\DeepLException;
+use DeepL\Translator;
+
+class SupportedLanguages
+{
+    /**
+     * @var DeepL\Translator
+     */
+    private $translator;
+
+    /**
+     * @var array<DeepL\Language>
+     */
+    private static $supportedLanguages;
+
+    private const must_have_country = [
+        'en' => 'en-gb',
+        'pt' => 'pt-pt'
+    ];
+
+    /**
+     * @param DeepL\Translator $translator
+     */
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param string $locale
+     * @return string
+     */
+    public function getAvailableLocale($locale): string
+    {
+        $locale = strtolower($locale);
+
+        if (isset(self::must_have_country[$locale])) {
+            return self::must_have_country[$locale];
+        }
+
+        $supportedLocales = $this->getAvailableLanguages();
+
+        if (preg_match("/^([a-z]{2,})-[a-z]{2,}$/", $locale, $matches)) {
+            $localeCountry = $locale;
+            $locale = $matches[1];
+        }
+
+        if (isset($localeCountry) && isset($supportedLocales[$localeCountry])) {
+            return $localeCountry;
+        }
+
+        if (isset($supportedLocales[$locale])) {
+            return $locale;
+        }
+
+        throw new DeepLException(
+            'Supported languages are: ' . implode(', ', $supportedLocales) .
+            ' Not `'. $locale .'`'
+        );
+    }
+
+    /**
+     * @return array<DeepL\Language>
+     */
+    private function getAvailableLanguages(): array
+    {
+        if (self::$supportedLanguages !== null) {
+            return self::$supportedLanguages;
+        }
+
+        self::$supportedLanguages = [];
+
+        /** @var DeepL\Language $language */
+        foreach ($this->translator->getTargetLanguages() as $language) {
+            $locale = strtolower($language->code);
+            self::$supportedLanguages[$locale] = $language;
+        }
+
+        return self::$supportedLanguages;
+    }
+}
