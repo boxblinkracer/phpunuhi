14224870f1c5c116cee57a462c71d7d722223e88
Philip Standt <philip.standt@strix.net>
2024-09-03 08:49:16 +0200

    feat: load dotenv files from current working directory

--- src/index.php
+++ src/index.php
@@ -1,8 +1,13 @@
 <?php
 
 use PHPUnuhi\AppManager;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once "phar://phpunuhi.phar/vendor/autoload.php";
 
+$envFile = getcwd() . '/.env';
+if (file_exists($envFile)) {
+    (new Dotenv())->usePutenv()->loadEnv($envFile);
+}
 
 AppManager::run();
